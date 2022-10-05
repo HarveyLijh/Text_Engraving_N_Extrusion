a161bcd49a11115762d704d160cf86544966b6f1
Harveylijh <jli783@wisc.edu>
2022-10-05 12:46:18 -0700

    cube engraving done

--- src/client/client.ts
+++ src/client/client.ts
@@ -6,15 +6,13 @@ import { Font, FontLoader } from 'three/examples/jsm/loaders/FontLoader'
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry'
 import { CSG } from './utils/CSGMesh'
 import * as TWEEN from '@tweenjs/tween.js'
-import Bender from './utils/bender'
-import  * as helvetiker_regular from './assets/fonts/helvetiker_regular.typeface.json'
+import * as helvetiker_regular from './assets/fonts/helvetiker_regular.typeface.json'
 
-const bender = new Bender()
 
 const scene = new THREE.Scene()
 // scene.background = new THREE.Color('skyblue' )
 const light1 = new THREE.SpotLight()
-light1.position.set(6.5, 7.5, 7.5)
+light1.position.set(53.2, -30.4, -39.3)
 light1.angle = Math.PI / 4
 light1.penumbra = 0.5
 scene.add(light1)
@@ -24,7 +22,8 @@ light2.position.set(-6.5, 7.5, 7.5)
 light2.angle = Math.PI / 4
 light2.penumbra = 0.5
 scene.add(light2)
-
+const light = new THREE.AmbientLight(0x404040); // soft white light
+scene.add(light);
 const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
@@ -33,7 +32,7 @@ const camera = new THREE.PerspectiveCamera(
 )
 camera.position.x = -2
 camera.position.y = 1
-camera.position.z = -2
+camera.position.z = 8
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
@@ -45,43 +44,42 @@ controls.target.z = -5
 
 const data = {
     text: 'abc123',
+    cube_scale_x: 5,
+    cube_scale_y: 5,
+    cube_scale_z: 5
 }
 
 const material = new THREE.MeshStandardMaterial({
-    metalness: .5,
-    roughness: .5,
+    metalness: .2,
+    roughness: 0,
     color: '#049ef4',
 })
-
-const cylinderMesh1 = new THREE.Mesh(
-    new THREE.CylinderGeometry(6, 6, 1.5, 64, 1, false),
+const material2 = new THREE.MeshStandardMaterial({
+    metalness: .2,
+    roughness: 0,
+    color: 'red',
+})
+// create cube to be engraved
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(data.cube_scale_x, data.cube_scale_y, data.cube_scale_z),
     material
 )
-const cylinderMesh2 = new THREE.Mesh(
-    new THREE.CylinderGeometry(5, 5, 1.6, 64, 1, false),
-    material
-)
-cylinderMesh1.position.set(0, 0, 0)
-cylinderMesh2.geometry.rotateX(-Math.PI / 2)
-cylinderMesh2.position.set(0, 0, 0)
-cylinderMesh2.geometry.rotateX(-Math.PI / 2)
-
-const cylinderCSG1 = CSG.fromMesh(cylinderMesh1)
-const cylinderCSG2 = CSG.fromMesh(cylinderMesh2)
-
-const ringCSG = cylinderCSG1.subtract(cylinderCSG2)
-const ringMesh = CSG.toMesh(ringCSG, new THREE.Matrix4())
-
-let engravedMesh = new THREE.Mesh(ringMesh.geometry, material)
+const cubeCSG = CSG.fromMesh(cube)
+const originMeshCSG = cubeCSG
+const originMesh = CSG.toMesh(originMeshCSG, new THREE.Matrix4())
+let engravedMesh = new THREE.Mesh(originMesh.geometry, material)
 scene.add(engravedMesh)
+
 let font: Font
 
+// load font and generate text mesh
 const loader = new FontLoader()
 loader.load(helvetiker_regular, function (f) {
     font = f
     regenerateGeometry()
 })
 
+// shift camera poisiton when resizing window
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight
@@ -89,57 +87,41 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight)
 }
 
-const raycaster = new THREE.Raycaster()
-
-renderer.domElement.addEventListener('dblclick', onDoubleClick, false)
-function onDoubleClick(event: MouseEvent) {
-    const mouse = {
-        x: (event.clientX / renderer.domElement.clientWidth) * 2 - 1,
-        y: -(event.clientY / renderer.domElement.clientHeight) * 2 + 1,
-    }
-
-    raycaster.setFromCamera(mouse, camera)
-
-    const intersects = raycaster.intersectObject(engravedMesh, false)
-    if (intersects.length > 0) {
-        const p = intersects[0].point
-        new TWEEN.Tween(controls.target)
-            .to(
-                {
-                    x: p.x,
-                    y: p.y,
-                    z: p.z,
-                },
-                200
-            )
-            .easing(TWEEN.Easing.Cubic.Out)
-            .start()
-    }
-}
-
+//  show stats gui to change text
 const stats = Stats()
 document.body.appendChild(stats.dom)
 
 const gui = new GUI()
-gui.add(data, 'text').onFinishChange(regenerateGeometry)
+
+const lightFolder = gui.addFolder('Ambient Light')
+lightFolder.add(light, 'intensity', 1, 10, 0.5)
+lightFolder.open()
+
+const textFolder = gui.addFolder('Text Engraving')
+textFolder.add(data, 'text').onFinishChange(regenerateGeometry)
+textFolder.open()
+
 gui.open()
 
+//  change text engraving based on gui input
 function regenerateGeometry() {
-    let newGeometry
+    let textGeometry
 
-    newGeometry = new TextGeometry(data.text, {
+    textGeometry = new TextGeometry(data.text, {
         font: font,
         size: 1,
         height: 0.2,
         curveSegments: 2,
     })
 
-    newGeometry.center()
-    bender.bend(newGeometry, 'y', Math.PI / 16)
-    newGeometry.translate(0, 0, -5)
+    textGeometry.center()
+    textGeometry.translate(0, 0, 2.4)
+    // const textMesh = new THREE.Mesh(textGeometry, material2);
+    // scene.add(textMesh)
 
-    const textCSG = CSG.fromGeometry(newGeometry)
-    const engravedCSG = ringCSG.subtract(textCSG)
+    //  reduce the text CSG out of out the original mesh to engrave
+    const textCSG = CSG.fromGeometry(textGeometry)
+    const engravedCSG = originMeshCSG.subtract(textCSG)
     engravedMesh.geometry.dispose()
     engravedMesh.geometry = CSG.toMesh(
         engravedCSG,
